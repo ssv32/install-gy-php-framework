cc45ca0c4976e3bb82e0f339b66dc4bb2973c979
ssv32 <ssv_32@mail.ru>
2020-09-11 00:32:19 +0600

    fix, add installDemoSite1.php

--- graphicalInstallGyFramework.php
+++ graphicalInstallGyFramework.php
@@ -824,7 +824,8 @@ if(isset($data['step']) && in_array($data['step'], $steps)){
             // установить демо сайт 1, если надо
             if(($err == false) && ($data['demo-site1'] == 'on') ){
                 global $argv;
-                $argv = 1;
+                $argv[] = 1;
+                $argv[] = 'start';
                 ob_start();
                 include 'gy/install/installDemoSite1.php';
                 $consoleLog = ob_get_contents();
